a2b16563c69e059d9e553e695f48a0c93f14c4ee
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-21 22:42:18 +0100

    formatting

--- src/main/groovy/trampoline/FactorialTrampoline.groovy
+++ src/main/groovy/trampoline/FactorialTrampoline.groovy
@@ -3,8 +3,9 @@ package trampoline
  * Created by mtumilowicz on 2018-11-21.
  */
 class FactorialTrampoline {
-    private static final def factorial = { n, accu = 1G -> 
-        n < 2 ? accu : factorial.trampoline(n - 1, n * accu) }
+    private static final def factorial = { n, accu = 1G ->
+        n < 2 ? accu : factorial.trampoline(n - 1, n * accu)
+    }
 
     static def getFactorial(int n) {
         return factorial(n)()
